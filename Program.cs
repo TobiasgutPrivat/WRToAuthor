249a458a390a5e735562072f027240a736658083
TobiasgutPrivat <79203201+TobiasgutPrivat@users.noreply.github.com>
2025-10-21 15:37:58 +0200

    add mapupload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,21 @@
 ﻿using GBX.NET;
-using GBX.NET.Engines.Game;
 using GBX.NET.LZO;
-using System.Text.Json;
 
 Gbx.LZO = new MiniLZO();
 // Gbx.ZLib = new ZLib();
 
-string? path = "C:/Users/tobia/Documents/Trackmania/Maps/Downloaded/CAN'T FIND A NAME, DEAL WITH IT.Map.Gbx";
+string? path = null; // set to null for command line .exe
 if (path == null)
 {
     if (args.Length > 0)
     {
         path = args[0];
-        if (System.IO.Directory.Exists(path))
+        Console.WriteLine("This will overwrite and upload directly, continue? (Y/N)");
+        bool overwrite = Console.ReadKey().Key == ConsoleKey.Y;
+        if (!overwrite)
         {
-            foreach (string file in System.IO.Directory.EnumerateFiles(path, "*.Map.Gbx"))
-            {
-                WRtoAuthor.setWRAuthor(file);
-            }
-        }
-        else if (System.IO.File.Exists(path))
-        {
-            WRtoAuthor.setWRAuthor(path);
-        }
-        else
-        {
-            Console.WriteLine("File or folder not found: " + path);
-            Environment.Exit(1);
+            Console.WriteLine("/nOperation cancelled.");
+            Environment.Exit(0);
         }
     }
     else
@@ -35,3 +24,29 @@ if (path == null)
         Environment.Exit(1);
     }
 }
+
+
+Console.WriteLine("Enter email: ");
+string email = Console.ReadLine() ?? "";
+Console.WriteLine("Enter password: ");
+string password = Console.ReadLine() ?? "";
+
+if (Directory.Exists(path))
+{
+    foreach (string file in Directory.EnumerateFiles(path, "*.Map.Gbx"))
+    {
+        WRtoAuthor.setWRAuthor(file, email, password);
+    }
+}
+else if (File.Exists(path))
+{
+    WRtoAuthor.setWRAuthor(path, email, password);
+}
+else
+{
+    Console.WriteLine("File or folder not found: " + path);
+    Environment.Exit(1);
+}
+
+Console.WriteLine("Done...");
+Console.ReadLine();
--- WrToAuthor.cs
+++ WrToAuthor.cs
@@ -1,23 +1,38 @@
-using System.Text.Json;
 using GBX.NET;
 using GBX.NET.Engines.Game;
+using ManiaAPI.NadeoAPI;
+using ManiaAPI.NadeoAPI.Extensions.Gbx;
+using TmEssentials;
 
 class WRtoAuthor
 {
-    public static void setWRAuthor(string path)
+    public static async void setWRAuthor(string path, string email, string password)
     {
+        using var nls = new NadeoLiveServices();
+        nls.AuthorizeAsync(email, password, AuthorizationMethod.UbisoftAccount).GetAwaiter().GetResult();
+        using var ns = new NadeoServices();
+        ns.AuthorizeAsync(email, password, AuthorizationMethod.UbisoftAccount).GetAwaiter().GetResult();
+
         Gbx<CGameCtnChallenge> gbx = Gbx.Parse<CGameCtnChallenge>(path);
         CGameCtnChallenge map = gbx.Node;
         string mapUid = map.MapInfo.Id;
 
-        JsonElement wr = API.getWR(mapUid).GetAwaiter().GetResult() ?? throw new Exception("No WR found");
-        string accountId = wr.GetProperty("accountId").GetString();
-        int score = wr.GetProperty("score").GetInt32();
+        MapInfoLive mapInfo = nls.GetMapInfoAsync(mapUid).GetAwaiter().GetResult();
+        Guid mapId = mapInfo.MapId;
 
-        map.AuthorLogin = accountId;
-        map.AuthorTime = new TmEssentials.TimeInt32(score);
-        map.AuthorScore = score;
-        gbx.Save(path + "modified.map.Gbx");
+        TopLeaderboardCollection leaderboard = nls.GetTopLeaderboardAsync(mapUid, 1).GetAwaiter().GetResult();
+        Record wr = leaderboard.Tops.First().Top.First();
+        map.AuthorLogin = Convert.ToBase64String(wr.AccountId.ToByteArray());
+        map.AuthorTime = wr.Score;
+        map.GoldTime = new TimeInt32((int)Math.Floor(wr.Score.TotalMilliseconds * 0.00106 + 1) * 1000);
+        map.SilverTime = new TimeInt32((int)Math.Floor(wr.Score.TotalMilliseconds * 0.0012 + 1) * 1000);
+        map.BronzeTime = new TimeInt32((int)Math.Floor(wr.Score.TotalMilliseconds * 0.0015 + 1) * 1000);
+        map.AuthorScore = wr.Score.TotalMilliseconds;
+        gbx.Save(path);
+        Console.WriteLine($"{path} Author set to {wr.AccountId} {wr.Score}");
 
+        using var fs = File.OpenRead(path);
+        ns.UpdateMapAsync(mapId, fs, Path.GetFileName(fs.Name)).GetAwaiter().GetResult();
     }
+    
 }
